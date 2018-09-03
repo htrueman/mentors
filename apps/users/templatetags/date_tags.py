78910ba85583d554aba4f9fc5f5d4d54c3d2e317
Vadym Hevlich <vege1wgw@gmail.com>
2018-09-03 15:06:09 +0300

    mentor office done

--- apps/users/templatetags/date_tags.py
+++ apps/users/templatetags/date_tags.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
+from dateutil.tz import tzlocal
 from django import template
 
 register = template.Library()
@@ -9,6 +10,24 @@ register = template.Library()
 @register.filter
 def get_age(date_of_birth):
     """
-    Returns age from a provided date of birth
+    Return age from a provided date of birth.
     """
     return (datetime.now().date() - relativedelta(years=date_of_birth.year)).year
+
+
+@register.filter
+def get_time_spent(datetime_value):
+    """
+    Return time spent value if it was spent less then 24 hours,
+    otherwise return datetime_value.
+    """
+    time_delta = datetime.now(tzlocal()) - datetime_value.replace(tzinfo=tzlocal())
+    time_delta_seconds = time_delta.seconds
+    if time_delta_seconds < 60:
+        return '{} сек тому'.format(time_delta_seconds)
+    elif 60 <= time_delta_seconds < 60 * 60:
+        return '{} хв тому'.format(time_delta_seconds // 60)
+    elif 60 * 60 <= time_delta_seconds < 60 * 60 * 24:
+        return '{} год тому'.format(time_delta_seconds // (60 * 60))
+    else:
+        return datetime_value
