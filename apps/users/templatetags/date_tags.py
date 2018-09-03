c2044314f0ba0d833a0c240736ac8933dab1d2ea
Vadym Hevlich <vege1wgw@gmail.com>
2018-09-03 17:27:50 +0300

    get_time_spent tag fixed

--- apps/users/templatetags/date_tags.py
+++ apps/users/templatetags/date_tags.py
@@ -21,13 +21,14 @@ def get_time_spent(datetime_value):
     Return time spent value if it was spent less then 24 hours,
     otherwise return datetime_value.
     """
-    time_delta = datetime.now(tzlocal()) - datetime_value.replace(tzinfo=tzlocal())
+    time_delta = datetime.now(tzlocal()) - datetime_value
     time_delta_seconds = time_delta.seconds
-    if time_delta_seconds < 60:
-        return '{} сек тому'.format(time_delta_seconds)
-    elif 60 <= time_delta_seconds < 60 * 60:
-        return '{} хв тому'.format(time_delta_seconds // 60)
-    elif 60 * 60 <= time_delta_seconds < 60 * 60 * 24:
-        return '{} год тому'.format(time_delta_seconds // (60 * 60))
+    if time_delta.days < 1:
+        if time_delta_seconds < 60:
+            return '{} сек тому'.format(time_delta_seconds)
+        elif 60 <= time_delta_seconds < 60 * 60:
+            return '{} хв тому'.format(time_delta_seconds // 60)
+        elif 60 * 60 <= time_delta_seconds < 60 * 60 * 24:
+            return '{} год тому'.format(time_delta_seconds // (60 * 60))
     else:
-        return datetime_value
+        return datetime_value.strftime('%d.%m.%Y')
