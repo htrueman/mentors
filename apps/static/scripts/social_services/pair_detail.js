0cf2a7cba77220ba344c2e79ba0ba6cd653fec0f
Vadym Hevlich <vege1wgw@gmail.com>
2018-11-20 19:44:35 +0200

    pair card edit frontend done

--- apps/static/scripts/social_services/pair_detail.js
+++ apps/static/scripts/social_services/pair_detail.js
@@ -5,15 +5,22 @@ const vm = new Vue({
     pairData: {},
     startDate: '',
     endDate: '',
-    publicServices: {},
+    publicServices: [
+      {
+        'pk': '',
+        'name': ''
+      }
+    ],
     mentorStatuses: {},
     organizations: [],
     statuses: {},
     viewMode: true,
+    soc_service_id: '',
     errors: {}
   },
   created() {
     $.get('?get_pair_data', (res) => {
+      this.soc_service_id = soc_service_id;
       this.pairData = res.pair_data;
       this.startDate = res.interaction_dates.start_date;
       this.endDate = res.interaction_dates.end_date;
@@ -26,6 +33,13 @@ const vm = new Vue({
   methods: {
     submit() {
 
+    },
+    findPublicService() {
+      const publicService = this.publicServices.find(p => p.pk === this.pairData.responsible)
+      if (publicService) {
+        return publicService.name
+      }
+      return ''
     }
   }
 });
